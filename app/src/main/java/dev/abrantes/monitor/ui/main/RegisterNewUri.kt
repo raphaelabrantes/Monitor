4a9de56a01395835770770158474629b16a521ca
raphaelabrantes <antenabr2@gmail.com>
2022-08-27 16:54:14 +0800

    URL local validation added

--- app/src/main/java/dev/abrantes/monitor/ui/main/RegisterNewUri.kt
+++ app/src/main/java/dev/abrantes/monitor/ui/main/RegisterNewUri.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import dev.abrantes.monitor.MonitorApplication
 import dev.abrantes.monitor.databinding.FragmentRegisterNewUriBinding
 
-class RegisterNewUri: Fragment() {
+class RegisterNewUri : Fragment() {
 
     private var _binding: FragmentRegisterNewUriBinding? = null
     private val binding get() = _binding!!
@@ -30,10 +31,30 @@ class RegisterNewUri: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-      binding.submitNewUrl.setOnClickListener {
-            val action = RegisterNewUriDirections.actionRegisterNewUriToMainFragment()
-            view.findNavController().navigate(action)
+        binding.submitNewUrl.setOnClickListener {
+            val uri = binding.healthUrl.text.toString()
+            if(checkValidUri(uri)){
+                val action = RegisterNewUriDirections.actionRegisterNewUriToMainFragment()
+                view.findNavController().navigate(action)
+            }
         }
+    }
+
+    private fun checkValidUri(uri: String): Boolean {
+        var schema = ""
+        if (uri.isEmpty()) {
+            binding.healthUrl.error = "Empty url"
+            return false
+        }
+        if (!(uri.startsWith("http://", true) || uri.startsWith("https://", true))) {
+            schema = if (binding.httpsSwitch.isActivated) "https://" else "http://"
+        }
+        if (!URLUtil.isValidUrl(schema + uri)) {
+            binding.healthUrl.error = "This url is not valid"
+            return false
+        }
+        binding.healthUrl.error = ""
+        return true
     }
 
     override fun onDestroyView() {
